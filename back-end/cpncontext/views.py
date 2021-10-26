332fab4b90a5eb1d9d58fc2f0de3eb0de32d8b6f
quypham1234 <quypham1503@gmail.com>
2021-10-26 23:44:50 +0700

    Update 26/10

--- back-end/cpncontext/views.py
+++ back-end/cpncontext/views.py
@@ -1,3 +1,4 @@
+import re
 from django.core.exceptions import ValidationError
 from rest_framework.serializers import Serializer
 from rest_framework import exceptions
@@ -22,3 +23,37 @@ class cpncontextAPIView(APIView):
 				# return render(request, 'ContextOfSmartContract.vue', {'cpncontext': serializeLTLpro}, status= status.HTTP_202_ACCEPTED)
 		except Exception as e: 
 			return Response({"message": "Get Data Fail!!"}, status=status.HTTP_400_BAD_REQUEST)
+	
+	def post(self,request):
+		try:
+			if request.method == 'POST':
+				serializeContext = cpncontextSerializer(data=request.data)
+				if serializeContext.is_valid():
+					serializeContext.save()
+					return Response({"message":"Created"},status=status.HTTP_201_CREATED)
+				return Response({"message":"Create fail!!!"},status=status.HTTP_400_BAD_REQUEST)
+		except Exception:
+			return Response({"message":"A"},status=status.HTTP_400_BAD_REQUEST)
+	
+	def put(self,request):
+		try:
+			if request.method == 'PUT':
+				idContext = request.data['id']
+				contextById = cpncontext.objects.get(id=idContext)
+				serializeUpdate = cpncontextSerializer(instance=contextById, data=request.data)
+				if serializeUpdate.is_valid():
+					serializeUpdate.save()
+					return Response({"message":"Update Successfully!!!"},status=status.HTTP_202_ACCEPTED)
+				return Response({"message":"SmartConstract Data Invalid!!!"},status=status.HTTP_409_CONFLICT)
+		except:
+			return Response({"message":"Fail!!"},status=status.HTTP_404_NOT_FOUND)
+	
+	def delete(self,request):
+		try:
+			if request.method =='DELETE':
+				idContextDelete = request.data['id']
+				contextDelete = cpncontext.objects.get(id=idContextDelete)
+				contextDelete.delete()
+				return Response('Success',status=status.HTTP_200_OK)
+		except:
+			return Response({"message":"Fail!!"},status=status.HTTP_400_BAD_REQUEST)
