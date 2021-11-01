72782c8bb9e8239193829243d0ac4db367c34b42
vinhhq123 <vinhhqse05532@fpt.edu.vn>
2021-11-01 10:49:56 +0700

    01/11(14)

--- back-end/smartconstract/urls.py
+++ back-end/smartconstract/urls.py
@@ -10,5 +10,14 @@ urlpatterns = [
     path('localvariable',views.getLocalVar),
     path('getfunction',views.getFuntionByScId),
     path('getvariablefunctionargu',views.getFunctionVarArgu),
+    path('addnewinitialmarking',views.addNewInitialMarking),
+    path('addnewbalancedtypefixed',views.addNewBalanceTypeFixed),
+    path('addnewbalancedtyperandom',views.addNewBalanceTypeRandom),
+    path('addnewimfunction',views.addNewIMFunction),
+    path('addnewimargument',views.addNewIMArgument),
+    path('addnewlnafile',views.addNewLNAFile),
+    path('addnewcheckedbatchsc',views.addNewCheckedBatchSC),
+    path('addnewcheckedsmartcontractdetail',views.addNewCheckedSmartContractDetail),
+    path('addnewbalance',views.addNewBalance),
     path('getargubyfunctionid', views.getArguByFunctionId)
 ]
