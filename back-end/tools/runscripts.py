771b06927474d37a6a621f6d30328fcfb0084d05
vinhhq123 <vinhhqse05532@fpt.edu.vn>
2021-11-08 08:30:44 +0700

    08/11(1)

--- back-end/tools/runscripts.py
+++ back-end/tools/runscripts.py
@@ -5,11 +5,7 @@ from subprocess import Popen, PIPE, STDOUT
 
 
 # for Windows
-<<<<<<< HEAD
 path = r"D:\Solidity\SolidityNew\SolidityProject\tools"
-=======
-path = r"D:\Demo\SolidityProject\tools"
->>>>>>> origin/sprint#2
 
 # for Ubuntu
 # path = r"G:\CapstoneProject\SolidityProject\tools"
