874a7b15714601fd8ae10edbb3dbf0a2c7d84e14
vinhhq123 <vinhhqse05532@fpt.edu.vn>
2021-11-08 23:13:40 +0700

    update test smartcontract

--- back-end/smartconstract/tests.py
+++ back-end/smartconstract/tests.py
@@ -1,7 +1,7 @@
-from django.test import TestCase
+from django.test import TestCase,TransactionTestCase
 from django.http import response
 from rest_framework import status
-
+#import pytest
 
 class SimpleTest(TestCase):
     # test get all smartcontract
@@ -32,80 +32,101 @@ class SimpleTest(TestCase):
             'http://127.0.0.1:8000/smartconstract/select-smart-contract?id=6')
 
 
-class TestFinalInsert(TestCase):
-    # test new insert into initialmanrking
+class TestInsertInitialMarking(TransactionTestCase):
+   # test new insert into initialmanrking
     def test_insert_into_initialmarking(self):
-        response = self.client.post(
-            'http://127.0.0.1:8000/smartconstract/addnewinitialmarking',
-            data={'num_user': '5', 'IM_type': 'new'},
-            content_type='application/json')
-        self.assertEquals(response.status_code, status.HTTP_201_CREATED)
+       response = self.client.post(
+           'http://127.0.0.1:8000/smartconstract/addnewinitialmarking',
+           data={'num_user': '5', 'IM_type': 'new'},
+           content_type='application/json')
+       self.assertEquals(response.status_code, status.HTTP_201_CREATED)
 
-class TestInsertIntoBalance(TestCase):
     # test new insert into balance
     def test_insert_into_Balance(self):
         response = self.client.post(
             'http://127.0.0.1:8000/smartconstract/addnewbalance',
-            data={'balanceType': 'Fixed', 'blvalue': '14', 'blfrom': '', 'blto': '', 'blrange': ''},
+            data={'balanceType': 'Fixed',  'blfrom': ' ', 'blto': ' ','blvalue': '14', 'blrange': ' '},
             content_type='application/json')
         self.assertEquals(response.status_code, status.HTTP_201_CREATED)
 
-class TestInsertIntoIMFunction(TestCase):
-    # LOI TESTCASE  , CON BEN POSTMAN CHAY OKE
+    # test new insert into IMFunction
     def test_insertIntoIMFunctions(self):
-        response = self.client.post(
-                                    'http://127.0.0.1:8000/smartconstract/addnewimfunction',
-                                    data={
-                                        'fun_name': 'test', 'sender_from': '1', 'sender_to': '10'},
-                                    content_type='application/json')
-        self.assertEquals(response.status_code, status.HTTP_201_CREATED)
+       response = self.client.post(
+                                   'http://127.0.0.1:8000/smartconstract/addnewimfunction',
+                                   data={'fun_name': 'test', 'sender_from': '1', 'sender_to': '10'},
+                                   content_type='application/json')
+       self.assertEquals(response.status_code, status.HTTP_201_CREATED)
 
+    # test new insert into IMArgument
     def test_insertIntoIMArgument(self):
-        response = self.client.post(
-                                    'http://127.0.0.1:8000/smartconstract/addnewimargument',
-                                    data={
-                                        'arg_name': 'test', 'IMfrom': '1', 'IMto': '10'},
-                                    content_type='application/json')
-        self.assertEquals(response.status_code, status.HTTP_201_CREATED)
+       response = self.client.post(
+                                   'http://127.0.0.1:8000/smartconstract/addnewimargument',
+                                   data={
+                                       'arg_name': 'test', 'IMfrom': '1', 'IMto': '10'},
+                                   content_type='application/json')
+       self.assertEquals(response.status_code, status.HTTP_201_CREATED)
+
+    # test new insert into CheckedBashSC
+    def test_insertIntoCheckBashSC(self):
+       response = self.client.post(
+                                   'http://127.0.0.1:8000/smartconstract/addnewcheckedbatchsc',
+                                   data={
+                                       'aid': '1', 'lteid': '1', 'cid': '1', 'noSC': '4',
+                                        'status': '1', 'LTLformula': 'test', 'result': 'test'},
+                                   content_type='application/json')
+       self.assertEquals(response.status_code, status.HTTP_201_CREATED)
+
+    # test new insert into CheckedSmartContractDetail
+    def test_insertIntoCheckSmartContractDetail(self):
+       response = self.client.post(
+                                   'http://127.0.0.1:8000/smartconstract/addnewcheckedsmartcontractdetail',
+                                   data={'sid': '1'},content_type='application/json')
+       self.assertEquals(response.status_code, status.HTTP_201_CREATED)
 
 class TestInsertLNAFile(TestCase):
     # test new insert into lnafile
     def test_insert_into_lnafile(self):
         response = self.client.post(
             'http://127.0.0.1:8000/smartconstract/addnewlnafile',
-            data={'hcpnfile': '"D:\Document\CapStone\CapstoneProject\SolidityProject\scripts\XMLfile\finnal_model.lna', 
-            'propfile': 'D:\Document\CapStone\CapstoneProject\SolidityProject\scripts\XMLfile\finnal_model.prop.lna'},
+            data={'hcpnfile': 'D:\\Solidity\\SolidityNew\\SolidityProject\\scripts\\XMLfile\\finnal_model.lna', 
+            'propfile': 'D:\\Solidity\\SolidityNew\\SolidityProject\\scripts\\XMLfile\\finnal_model.prop.lna'},
             content_type='application/json')
         self.assertEquals(response.status_code, status.HTTP_201_CREATED)
 
-# class TestAllGetFunction(TestCase):
-# LOI 400 , DO DUNG SERIALIER ??????
-#    def test_get_sc_by_Id(self):
-#        data = {'id':'4'}
-#        response =  self.client.get('http://127.0.0.1:8000/smartconstract/scbyid',data=data)
-#        self.assertEqual(response.status_code, status.HTTP_200_OK)
+# DANG LOI
+# class TestGetSCById(TestCase):
+#     # BEN  VIEWS.PY  DANG DUNG SERIALIZER
+#     # test get smartcontract by smartcontract ID
+#     def test_get_sc_by_Id(self):
+#          response =  self.client.get('http://127.0.0.1:8000/smartconstract/scbyid?id=2')
+#          self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-#    def test_get_globalvariable_by_SCId(self):
-#        data = {'id':'4'}
-#        response =  self.client.get('http://127.0.0.1:8000/smartconstract/globalvarialbe',data=data)
-#        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+class TestGetGlobalvariablebySCId(TestCase):
+    # test get global variable by smartcontract ID
+    def test_get_globalvariable_by_SCId(self):
+        response =  self.client.get('http://127.0.0.1:8000/smartconstract/globalvarialbe?id=4')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-#    def test_get_function_by_SCId(self):
-#        data = {'id':'4'}
-#        response =  self.client.get('http://127.0.0.1:8000/smartconstract/getfunction',data=data)
-#        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+class TestGetFunctionBySCId(TestCase):
+    # test get function by smartcontract ID
+    def test_get_function_by_SCId(self):
+        response =  self.client.get('http://127.0.0.1:8000/smartconstract/getfunction?id=4')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-#    def test_get_localVar_by_FunctionID(self):
-#        data = {'id':'1'}
-#        response =  self.client.get('http://127.0.0.1:8000/smartconstract/localvariable',data=data)
-#        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+class TestGetLocalValByFunctionId(TestCase):
+    # test get global variable by function ID
+    def test_get_localVar_by_FunctionID(self):
+        response =  self.client.get('http://127.0.0.1:8000/smartconstract/localvariable?id=1')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-#    def test_getFunctionVarArgu_by_SCId(self):
-#        data = {'id':'1'}
-#        response =  self.client.get('http://127.0.0.1:8000/smartconstract/getvariablefunctionargu',data=data)
-#       self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+class TestGetFunctionVarArguBySCId(TestCase):
+    # test get Function, Variable,Argument by smartcontract ID
+    def test_getFunctionVarArgu_by_SCId(self):
+        response =  self.client.get('http://127.0.0.1:8000/smartconstract/getvariablefunctionargu?id=1')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-#    def test_getArgument_by_FunctionId(self):
-#        data = {'id':'4'}
-#        response =  self.client.get('http://127.0.0.1:8000/smartconstract/getargubyfunctionid',data=data)
-#        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+class TestGetArgumentByFunctionId(TestCase):
+    # test get argument by function ID
+    def test_getArgument_by_FunctionId(self):
+        response =  self.client.get('http://127.0.0.1:8000/smartconstract/getargubyfunctionid?id=4')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
