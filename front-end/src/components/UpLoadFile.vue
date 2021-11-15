e0cdb90ee3336ae88d488378844a05138505273b
NguyenHongHanh301999 <hanhtrang301999@gmail.com>
2021-11-15 13:12:31 +0700

    update uploadFile

---
+++ front-end/src/components/UpLoadFile.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="container">
+    <div id="dialog">
+     
+        <h3>{{ this.dialog.title }}</h3>
+      
+      <div id="d-content">
+        <a-upload-dragger
+          name="file"
+          action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+          @change="handleChange"
+        >
+          <p class="ant-upload-drag-icon">
+            <a-icon type="inbox" />
+          </p>
+          <p class="ant-upload-text">Click or drag file to upload</p>
+        </a-upload-dragger>
+      </div>
+      <div id="btn-group">
+        <button class="btn btn-outline-primary btn-sm" @click="confirm">
+          {{ this.dialog.confirmbtn }}
+        </button>
+        <button class="btn btn-outline-primary btn-sm" @click="cancel">
+          No
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ["dialog"],
+  methods: {
+    cancel() {
+      this.$emit("cancel");
+    },
+    confirm() {
+      this.$emit("confirm");
+    },
+  },
+};
+</script>
+<style scoped>
+#dialog {
+  width: 500px;
+  height: 300px;
+ 
+  margin: auto;
+  background-color: white;
+  border-radius: 5px;
+  padding: 15px;
+}
+h3 { 
+  text-align: center;
+  padding: 10px;
+}
+#btn-group {
+  width: 50%;
+  float: right;
+  display: flex;
+  justify-content: space-evenly;
+  margin: 30px auto;
+}
+</style>
--- front-end/src/views/context/LoadContext.vue
+++ front-end/src/views/context/LoadContext.vue
@@ -1,43 +1,13 @@
 <template>
-  <div class="container-fluid">
-     <div class="row align-items-md-center">
-      <div class="col-2 ">
-        <span>
-          <a href="/" class="link-primary text-decoration-underline">Home</a> >
-         <a>Load</a></span
-        >
-      </div>
-      <div class="col-8 text-center"><h1 class="text-center">Choose a new Smart Contract file</h1></div>
-    </div>
-    <div  class="inputStyle" v-if="step =='initial'">
-    <input  type="file" @change="onFileChange" >
-    </div>
-    <div v-if="step == 'done'"> hi</div>
-  </div>
-  
- 
+ <h1>Load Context</h1>
 </template>
 
 <script>
 export default {
-  data() {
-    return{
-      smfile: '',
-     step : 'initial',
-    }
-  },
-  
-  methods: {
-    onFileChange() {
-      this.smfile = this.$refs.file.files[0];
-      this.step = 'done';
-    }
-  },
+ 
 };
 </script>
 
 <style>
-.inputStyle{
-margin-left: 25%;
-}
+
 </style>
