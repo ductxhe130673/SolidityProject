ef1c4c2e2963f6c0fc7693907917931cd17329d2
vinhhq123 <vinhhqse05532@fpt.edu.vn>
2021-12-23 23:31:41 +0700

    24/12

--- back-end/account/tests.py
+++ back-end/account/tests.py
@@ -3,14 +3,15 @@ from rest_framework import response, status
 # Create your tests here.
 
 
-class TestContactGetMethods(TestCase):
+class TestGetContactByAccountId(TestCase):
     # test get contact by account id
     def test_get_contact_by_accountId(self):
         response = self.client.get(
             'http://127.0.0.1:8000/auth/getcontactbyaid?id=1')
         self.assertEquals(response.status_code, status.HTTP_201_CREATED)
 
-class TestContactGetMethods1(TestCase):
+
+class TestGetAvartarByAccountId(TestCase):
     # test get avartar by account id
     def test_get_avatar_by_aid(self):
         response = self.client.get(
@@ -18,20 +19,37 @@ class TestContactGetMethods1(TestCase):
         self.assertEquals(response.status_code, status.HTTP_201_CREATED)
 
 
-# class TestInsertMethods(TransactionTestCase):
-#     # test update contact information
-#     def test_update_contact_info(self):
-#         response = self.client.put(
-#             'http://127.0.0.1:8000/auth/updatecontactbyaid',
-#             data={'firstname': 'leo', 'lastname': 'messi', 'email': 'messi@gmail.com',
-#                   'phone': '0987654321', 'birthDate': '1998-09-09',
-#                   'avartar': 'D:\\Solidity\\SolidityNew\\SolidityProject\\scripts\\image\\index2.jpeg', 'address': 'Paris', 'aid': '6'},
-#             content_type='application/json')
-#         self.assertEquals(response.status_code, status.HTTP_201_CREATED)
+class TestInsertMethods(TransactionTestCase):
+        # test update contact information
+        def test_update_contact_info(self):
+            response = self.client.put(
+                'http://127.0.0.1:8000/auth/updatecontactbyaid',
+                data={'firstname': 'leo', 'lastname': 'messi', 'email': 'messi@gmail.com',
+                      'phone': '0987654321', 'birthDate': '1998-09-09',
+                      'avartar': 'D:\\Solidity\\SolidityNew\\SolidityProject\\scripts\\image\\index2.jpeg', 'address': 'Paris', 'aid': '7'},
+                content_type='application/json')
+            self.assertEquals(response.status_code, status.HTTP_201_CREATED)
 
     # test insert into contact
     # def test_insert_into_contact(self):
     #     response = self.client.post(
     #         'http://127.0.0.1:8000/auth/insertintocontact',
     #         data={'email': 'test'}, content_type='application/json')
-    #     self.assertEquals(response.status_code, status.HTTP_201_CREATED)        
+    #     self.assertEquals(response.status_code, status.HTTP_201_CREATED)
+
+class TestPostMethods(TransactionTestCase):
+    # test register method
+    def testPostRegister(self):
+        response = self.client.post('http://127.0.0.1:8000/auth/register/',
+                                    data={
+                                        'username': 'testuser3', 'password': 'testpassword', 'role': 'user'},
+                                    content_type='application/json')
+        self.assertEquals(response.status_code, status.HTTP_201_CREATED)
+
+    # test login method
+    def testPostLogin(self):
+        response = self.client.post('http://127.0.0.1:8000/auth/login',
+                                    data={
+                                        'username': 'xuanduc', 'password': 'admin'},
+                                    content_type='application/json')
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
